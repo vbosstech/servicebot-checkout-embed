944c9793928cfcafe1ac43a06cffe2c691321a7d
bsears90 <bsears@vampeo.com>
2018-08-27 17:58:55 -0400

    added abilityu to customize message

--- src/forms/request-form.js
+++ src/forms/request-form.js
@@ -377,7 +377,7 @@ class ServiceInstanceForm extends React.Component {
             'method': 'POST',
             'url': `${this.props.url}/api/v1/service-templates/${this.props.templateId}/request`
         };
-        let successMessage = 'Purchase Completed! You should receive an email shortly.';
+        let successMessage = this.props.message || 'Request Successful';
         let successRoute = "/my-services";
         //If admin requested, redirect to the manage subscription page
         console.log(this.state.servicePrice, this.props.plan);
